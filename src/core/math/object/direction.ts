7710e535a605a453c240382b27387f7ddaa89949
rmkane <rmkane89@gmail.com>
2023-08-28 20:17:06 -0400

    fix: docs

--- src/core/math/object/direction.ts
+++ src/core/math/object/direction.ts
@@ -1,7 +1,13 @@
 import { Object2D } from '../../../types'
 
-function direction(a: Object2D): number {
-  return Math.atan2(a.y, a.x)
+/**
+ * TBD
+ *
+ * @param {Object2D} vector
+ * @returns {number}
+ */
+function direction(vector: Object2D): number {
+  return Math.atan2(vector.y, vector.x)
 }
 
 export { direction as directionObject }
--- src/core/math/object/dot.ts
+++ src/core/math/object/dot.ts
@@ -1,5 +1,12 @@
 import { Object2D } from '../../../types'
 
+/**
+ * TBD
+ *
+ * @param {Object2D} left
+ * @param {Object2D} right
+ * @returns {number}
+ */
 function dot(left: Object2D, right: Object2D): number {
   return left.x * right.x + left.y * right.y
 }
