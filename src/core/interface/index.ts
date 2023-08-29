dcfbaa8369302f4b0223e16d262f4ab1f65f591b
rmkane <rmkane89@gmail.com>
2023-08-29 12:39:32 -0400

    fix: update deps and fix exports

--- src/core/interface/index.ts
+++ src/core/interface/index.ts
@@ -1,4 +1,6 @@
-export { default as AbstractPoint } from './AbstractPoint'
-export { default as Point2D } from './Point2D'
-export { default as Point3D } from './Point3D'
-export { default as Vector2D } from './Vector2D'
+import AbstractPoint from './AbstractPoint'
+import Point2D from './Point2D'
+import Point3D from './Point3D'
+import Vector2D from './Vector2D'
+
+export { AbstractPoint, Point2D, Point3D, Vector2D }
