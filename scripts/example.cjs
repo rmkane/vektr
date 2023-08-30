005e7e0a699e493cfd547a81220848a7b0b3e9fb
rmkane <rmkane89@gmail.com>
2023-08-30 13:29:37 -0400

    feat: add more sample cjs usage

--- scripts/example.cjs
+++ scripts/example.cjs
@@ -1,3 +1,9 @@
+/*
+ * ============================================================================
+ * Example usage for Vector2D
+ * ============================================================================
+ */
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Vektr = require('../lib/index.js')
 
@@ -9,3 +15,31 @@ const v3 = v1.add(v2)
 
 // eslint-disable-next-line no-console
 console.log(v3.toJSON())
+
+/*
+ * ============================================================================
+ * Example usage for addObject
+ * ============================================================================
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { addObject } = require('../lib/core/math/object/operation/binary/add.js')
+
+const v4 = addObject(v1, v2, new Vector2D())
+
+// eslint-disable-next-line no-console
+console.log(v4.toJSON())
+
+/*
+ * ============================================================================
+ * Example usage for addObject
+ * ============================================================================
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { ObjectVectorMath2D } = require('../lib/core/math/index.js')
+
+const v5 = ObjectVectorMath2D.add(v1, v2, new Vector2D())
+
+// eslint-disable-next-line no-console
+console.log(v5.toJSON())
